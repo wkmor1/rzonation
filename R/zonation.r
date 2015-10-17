bb4bb3c125d900b30e64cc9d69ada60f58897dcd
wkmor1 <wkmor1@gmail.com>
2015-10-17 05:46:40 +0000

    zonation now has s4 methods for RasterStack and character vector (of raster files)

--- R/zonation.r
+++ R/zonation.r
@@ -1,128 +1,158 @@
-#'Run the program zonation from R
-#'
-#'Runs the zonation software for conservation planning on a set of GeoTiff files
-#'and returns the output as a list.
-#'
-#'@param features file paths of the GeoTiff files for features in the conservation plan.
-#'
-#'@export
+#"Run the program zonation from R
+#"
+#"Runs the zonation software for conservation planning on a set of GeoTiff files
+#"and returns the output as a list.
+#"
+#"@param features file paths of the GeoTiff files for features in the conservation plan.
+#"
+#"@export
 
-zonation                                                                      <-
-  function(features)                                                           {
+setGeneric(
+  "zonation",
+  function(features) {
+    standardGeneric("zonation")
+  }
+);
 
-  zp                                                                          <-
-    base::getOption('rzonation.path')                                          ;
+setMethod(
+   "zonation",
+   c(features = "RasterStack"),
+   function(features) {
+     raster::writeRaster(
+       x       = features,
+       file    =
+         base::paste0(
+           base::tempdir(),
+           "/", "feature.tif"
+         ),
+       bylayer = TRUE,
+       suffix  = "names"
+     );
+     rzonation::zonation(
+       features = base::paste0(
+         base::tempdir(),
+         "/", "feature_",
+         base::names(features),
+         ".tif"
+       )
+     );
+   }
+);
 
-  if (!base::nzchar(zp))                                                       {
-     base::stop('zonation binary not found')                                 ;};
+setMethod(
+  "zonation",
+  c(features = "character"),
+  function(features) {
+    zp <- base::getOption("rzonation.path");
+    if (!base::nzchar(zp)) base::stop("zonation binary not found");
+    dir <- base::tempdir();
+    datfile <- base::tempfile(tmpdir = dir);
 
-  dir                                                                         <-
-    base::tempdir()                                                            ;
+    base::paste(
+      "[Settings]",
+      "removal rule = 1",
+      "warp factor = 1000",
+      "edge removal = 1",
+      "add edge points = 0",
+      "annotate name = 0",
+      sep = "\n"
+    ) %>%
+    base::cat(file = datfile);
 
-  datfile                                                                     <-
-    base::tempfile(tmpdir = dir)                                               ;
+    spfile <- base::tempfile(tmpdir = dir);
 
-  base::paste(
-    '[Settings]',
-    'removal rule = 1',
-    'warp factor = 1000',
-    'edge removal = 1',
-    'add edge points = 0',
-    'annotate name = 0',
-    sep = '\n'
-  )                                                                          %>%
-  base::cat(file = datfile)                                                    ;
+    features %>%
+    base::paste0("1 1 1 1 1 ", ., "\n", collapse = "") %>%
+    base::cat(file = spfile);
 
-  spfile                                                                      <-
-    base::tempfile(tmpdir = dir)                                               ;
+    resstem  <- base::tempfile(tmpdir = dir);
 
-  features                                                                   %>%
-  base::paste0('1 1 1 1 1 ', ., '\n', collapse = '')                         %>%
-  base::cat(file = spfile)                                                     ;
+    base::paste(
+      base::getOption("rzonation.path"),
+      "-r",
+      datfile,
+      spfile,
+      resstem,
+      "0.0 0 1.0 1"
+    ) %>%
+    base::system(ignore.stdout = TRUE);
 
-  resstem                                                                     <-
-    base::tempfile(tmpdir = dir)                                               ;
+    nfeatures <- base::length(features);
 
-  base::paste(
-    base::getOption('rzonation.path'),
-    '-r',
-    datfile,
-    spfile,
-    resstem,
-    '0.0 0 1.0 1'
-  )                                                                          %>%
-  base::system(ignore.stdout = TRUE)                                           ;
+    features_info <-
+      base::paste0(resstem, ".features_info.txt")       %>%
+      base::scan(skip = 2, what = "char", quiet = TRUE) %>%
+      base::matrix(nrow = nfeatures, byrow = TRUE)      %>%
+      base::as.data.frame(stringsAsFactors = FALSE)     %>%
+      readr::type_convert(.) %>%
+      magrittr::set_colnames(
+        base::c(
+          "weight",
+          "dist_sum",
+          "ig_retain",
+          "t_viol_fract_rem",
+          "dist_mean_x",
+          "dist_mean_y",
+          "map_file_name"
+        )
+      );
 
-  nfeatures                                                                   <-
-    features                                                                 %>%
-    base::length(x = .)                                                        ;
+    curves <-
+      readr::read_table(
+        file      = base::paste0(resstem, ".curves.txt"),
+        col_names = base::c(
+                      "prop_landscape_lost",
+                      "cost_need_for_top_frac",
+                      "min_prop_rem",
+                      "ave_prop_rem",
+                      "w_prop_rem",
+                      "ext_1",
+                      "ext_2",
+                      base::paste0(
+                        "prop_",
+                        tools::file_path_sans_ext(x = features),
+                        "_rem"
+                      )
+                    ),
+        col_types = {
+                    "d"                   %>%
+                    base::rep(nfeatures)  %>%
+                    base::c("diddddd", .) %>%
+                    base::paste0(collapse = "");
+                  },
+        skip      = 1
+      );
 
-  features_info                                                               <-
-    base::paste0(resstem, '.features_info.txt')                              %>%
-    base::scan(skip = 2, what = 'char', quiet = TRUE)                        %>%
-    base::matrix(nrow=nfeatures, byrow = TRUE)                               %>%
-    base::as.data.frame(stringsAsFactors = FALSE)                            %>%
-    readr::type_convert(.)                                                   %>%
-    magrittr::set_colnames(
-      base::c(
-        'weight',
-        'dist_sum',
-        'ig_retain',
-        't_viol_fract_rem',
-        'dist_mean_x',
-        'dist_mean_y',
-        'map_file_name'
-      )
-    )                                                                          ;
+    rasters <-
+      base::basename(path = resstem) %>%
+      base::list.files(
+        path = dir,
+        pattern = .,
+        full.names = TRUE
+      ) %>%
+      base::grep(
+        pattern = "\\.tif$",
+        x = .,
+        value = TRUE
+      ) %>%
+      raster::stack(x = .) %>%
+      magrittr::set_names(
+        value = base::c("rank", "wrscr")
+      ) %>%
+      raster::readAll(object = .);
 
-  curves                                                                      <-
-    readr::read_table(
-      file      = base::paste0(resstem, '.curves.txt'),
-      col_names = base::c(
-                    'prop_landscape_lost',
-                    'cost_need_for_top_frac',
-                    'min_prop_rem',
-                    'ave_prop_rem',
-                    'w_prop_rem',
-                    'ext_1',
-                    'ext_2',
-                    base::paste0(
-                      'prop_',
-                      tools::file_path_sans_ext(x = features),
-                      '_rem'
-                    )
-                  ),
-      col_types =                                                              {
-                  'd'                                                        %>%
-                  base::rep(nfeatures)                                       %>%
-                  base::c('diddddd', .)                                      %>%
-                  base::paste0(collapse = '')                                 ;}
-                  ,
-      skip      = 1
-    )                                                                          ;
+    run_info <-
+      readr::read_file(
+        file = base::paste0(resstem, ".run_info.txt")
+      );
 
-  rasters                                                                     <-
-    resstem                                                                  %>%
-    base::basename(path = .)                                                 %>%
-    base::list.files(path = dir, pattern = ., full.names = TRUE)             %>%
-    base::grep(pattern = '\\.tif$', x = ., value = TRUE)                     %>%
-    raster::stack(x = .)                                                     %>%
-    magrittr::set_names(
-      value = base::c('rank', 'wrscr')
-    )                                                                        %>%
-    raster::readAll(object = .)                                                ;
+    base::list(
+      features_info = features_info,
+      curves        = curves,
+      rasters       = rasters,
+      run_info      = run_info
+    );
+  }
+);
 
-  run_info                                                                    <-
-    readr::read_file(
-      file = base::paste0(resstem, '.run_info.txt')
-    )                                                                          ;
-
-  base::list(
-    features_info = features_info,
-    curves        = curves,
-    rasters       = rasters,
-    run_info      = run_info
-  )                                                                          ;};
-
-utils::globalVariables('weight')                                               ;
-
+utils::globalVariables("weight")
