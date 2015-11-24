49404f60f210e10f71ab84c1fc31f20b16efe84e
wkmor1 <wkmor1@gmail.com>
2015-11-24 02:07:16 +0000

    summary method added

---
+++ R/summary.zonation.r
@@ -0,0 +1,21 @@
+#' See output of zonation run
+#'
+#' See the run information produced by zonation
+#'
+#' @param object a zonation object.
+#' @param ... additional arguments affecting the summary produced.
+#'
+#' @examples
+#' library(raster)
+#' r1 <- raster(matrix(runif(200^2, 0, 1), 200))
+#' r2 <- raster(matrix(runif(200^2, 0, 1), 200))
+#' plan <- zonation(stack(r1, r2))
+#' library(ggplot2)
+#' summary(plan)
+#'
+#' @export
+
+summary.zonation <-
+  function(object, ...) {
+    cat(object$run_info)
+  }
