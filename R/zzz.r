dcc129a85b8e6ca80c4fcf261c3eb121e4935158
wkmor1 <wkmor1@gmail.com>
2015-11-26 01:08:08 +0000

    can now use zonation on windows

--- R/zzz.r
+++ R/zzz.r
@@ -1,24 +1,30 @@
 # Check for zig4 binary
 
-.onLoad                                                                       <-
-  function(libname, pkgname)                                                   {
-  op                                                                          <-
+.onLoad <-
+  function(libname, pkgname) {
+  op <-
     base::options()
-  op.rzonation                                                                 <-
+  op.rzonation <-
     base::list(
-      rzonation.path = base::Sys.which('zig4')
+      rzonation.path = {
+        if (.Platform$OS.type == "windows") {
+          base::Sys.which("zig4.exe")
+        } else {
+          base::Sys.which("zig4")
+        }
+      }
     )
-  toset                                                                       <-
-    op.rzonation                                                             %>%
-    base::names(.)                                                           %>%
+  toset <-
+    op.rzonation   %>%
+    base::names(.) %>%
     magrittr::is_in(
       x     = .,
       table = base::names(op)
-    )                                                                        %>%
-    magrittr::not(.)                                                           ;
-
-  if (base::any(toset))                                                        {
-    base::options(op.rzonation[toset])                                       ;};
-
-  base::invisible()                                                          ;};
+    ) %>%
+    magrittr::not(.);
+  if (base::any(toset)) {
+    base::options(op.rzonation[toset]);
+  };
+  base::invisible();
+  }
 
