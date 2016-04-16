0ae0ab3ea1a42b42ab70d17400945aa1670eea01
skiptoniam <skiptoniam@gmail.com>
2016-04-16 15:26:11 +1000

    added the capacity to return prop remaining for features to mean.rzonation - I need this for boxplots.

--- R/mean.zonation.r
+++ R/mean.zonation.r
@@ -4,6 +4,7 @@
 #'
 #' @param x a zonation object.
 #' @param p numeric proportion of landscape protected.
+#' @param features numeric vector of features id number (order features are added to rzonation)
 #' @param method the method of calculating the mean arithmetic or geometric.
 #' @param ... further arguments passed to or from other methods.
 #'
@@ -17,31 +18,39 @@
 #' @export
 
 mean.zonation <-
-  function(x, p = .1, method = c("arithmetic", "geometric"), ...) {
+  function(x, p = .1, features=NULL, method = c("arithmetic", "geometric"), ...) {
   if (!identical(length(p), 1L) || !base::is.numeric(p)) {
     base::stop("p must be scalar");
   }
   if (p == 0) return(0);
   if (p == 1) return(1);
   if (p < 0 || p > 1) base::stop("p must be a proportion");
-
+  if (is.null(features)){features<-"ave_prop_rem"
+  } else {
+    if (!base::is.numeric(features))base::stop("feature must be a numeric vector column indexs")
+    features <- features + 7
+  }
   row <-
     x$curves$prop_landscape_lost %>%
     magrittr::subtract(1 - p) %>%
     base::abs() %>%
     base::which.min() %>%
-    magrittr::extract(x$curves, ., );
+    magrittr::extract(x$curves, .,);
 
   base::match.arg(method) %>%
   base::switch(
     arithmetic =
-      row["ave_prop_rem"] %>%
+      row[features] %>%
       base::as.numeric(),
     geometric =
       row[-1:-7] %>%
       base::log() %>%
       base::as.numeric() %>%
-      base::mean(..., na.rm = TRUE) %>%
+      (function(x){
+        if(feature=='ave_prop_rem')
+          base::mean(x, na.rm = TRUE)
+        else x
+        }) %>%
       base::exp()
   );
 }
